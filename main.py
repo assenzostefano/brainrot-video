c532a3e4a36fa8538b5f9acad288a99bcaf844cc
Stefano Assenzo <assenzo.stefano@gmail.com>
2025-03-08 17:29:44 +0100

    video resized for social media

--- main.py
+++ main.py
@@ -6,22 +6,23 @@ text_for_video = ("Questo è un esempio di un testo molto lungo che vogliamo div
 
 def generate_video(text):
     clip = VideoFileClip("assets/background_video/barbazza.mp4")
-
+    clip_resized = clip.resized(height=1920).cropped(width=1080, x_center=clip.w)
+    #clip_vertical = clip_resized.cropped(clip_resized, width=1080, x_center=clip_resized.w/2)
     segment_duration = clip.duration / len(text)
 
     # Crea una lista di TextClip per ciascun segmento, impostando la durata e il tempo di inizio
     text_clips = []
     for i, seg in enumerate(text):
         txt_clip = TextClip(text=seg, 
-                            font="assets/font/Roboto-Black.ttf", 
-                            font_size=70, 
+                            font="assets/font/Roboto-Black.ttf",
+                            font_size=70,
                             color='black') \
                    .with_duration(segment_duration) \
                    .with_position("center") \
                    .with_start(segment_duration * i)
         text_clips.append(txt_clip)
         
-    final_clip = CompositeVideoClip([clip, *text_clips], size=(1920, 1080))
+    final_clip = CompositeVideoClip([clip_resized, *text_clips], size=(1080, 1920))
 
     final_clip.write_videofile("assets/output/barbazza.mp4", codec='libx264', fps=25)
 
